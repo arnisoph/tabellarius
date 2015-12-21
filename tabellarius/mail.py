2d8db6151ee1046c5731027f915141e3a2bb7c3a
Arnold Bechtoldt <mail@arnoldbechtoldt.com>
2015-12-21 18:21:52 +0100

    remove some comments

--- tabellarius/mail.py
+++ tabellarius/mail.py
@@ -6,66 +6,12 @@ import email.message
 import email.header
 import email.utils
 
-#class Mail(dict):
-#    mail_native = email.message.Message()
-#
-#    def __init__(self, logger, uid, mail=None):
-#        self.logger = logger
-#        self.uid = uid
-#        self.mail_native = mail
-#        self.parse_email_object()
-#
-#    def parse_email_object(self):
-#        fields_to_store = {
-#            # TODO store as much as possible, get rid of this static list
-#            'content-type': None,
-#            'date': None,
-#            'delivered-to': {'multiple': True},
-#            'from': None,
-#            'list-id': None,
-#            'message-id': None,
-#            'received': {'multiple': True,
-#                         'split': True},
-#            'return-path': None,
-#            'subject': None,
-#            'to': None,
-#            'user-agent': None,
-#            'x-redmine-project': None,
-#            'x-redmine-host': None,
-#            'x-gitlab-project': None,
-#        }
-#
-#        for field, properties in fields_to_store.items():
-#            if type(properties) is dict:
-#                if properties.get('multiple', None):
-#                    fields = self.mail_native.get_all(field)
-#                    if fields is None:
-#                        continue
-#                    #    self.logger.debug('Unable to parse raw mail with uid=%s: %s %s', self.uid, self.mail_native, field)
-#                    #    raise ValueError('Unable to parse raw mail with uid=%s' % (self.uid))
-#
-#                    if properties.get('split', None):
-#                        _fields = []
-#                        for f in fields:
-#                            _fields.append(f.split('\r\n'))
-#                        _fields = fields
-#                    self[field] = fields
-#                else:
-#                    field_value = self.mail_native.get(field)
-#                    if field_value:
-#                        self[field] = field_value
-#            else:
-#                field_value = self.mail_native.get(field)
-#                if field_value:
-#                    self[field] = field_value
-
 
 class Mail():
     """
     A dict representing a mail
     """
 
-    #def __init__(self, logger, charset='utf-8', mail_native=None, **kwargs):
     def __init__(self, logger, charset='utf-8', headers={}, body='', mail_native=None):
         self.logger = logger
         self.charset = charset
