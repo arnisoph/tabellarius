977e5cc927472ac6683ec4ba7eb890f9a030f8a9
Arnold Bechtoldt <arnold.bechtoldt@inovex.de>
2016-10-08 10:16:07 +0200

    drop python 3.3 support

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ setup(name='tabellarius',
           'License :: OSI Approved :: Apache Software License',
           'Programming Language :: Python',
           'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Topic :: Communications :: Email :: Filters',])
