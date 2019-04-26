37b724c2f2a6ed670a7b39ce6cbcba31858028da
Arnold Bechtoldt <arnold.bechtoldt@inovex.de>
2019-04-26 23:30:01 +0200

    release 2.0.1
    Signed-off-by: Arnold Bechtoldt <arnold.bechtoldt@inovex.de>

--- tabellarius/main.py
+++ tabellarius/main.py
@@ -12,7 +12,7 @@ from tabellarius.imap import IMAP
 from tabellarius.mail_filter import MailFilter
 from tabellarius.misc import ConfigParser, Helper
 
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 
 
 def main():
